509d118d6b24d84dfbb4a29027a67c1425d11e9c
KB <kb@kb.com>
2018-08-25 14:42:58 +0530

    Step 03 - Creating a Hello World Service

---
+++ src/main/java/com/personal/kunj/springbootrestfulservice/SpringbootRestfulServiceApplication.java
@@ -0,0 +1,12 @@
+package com.personal.kunj.springbootrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulServiceApplication.class, args);
+	}
+}
