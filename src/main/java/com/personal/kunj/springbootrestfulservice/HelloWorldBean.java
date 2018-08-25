0e6f97c0a59f32811425127b9e58f0d321d0a640
KB <kb@kb.com>
2018-08-25 15:51:54 +0530

    Step 04 - Enhancing the Hello World Service to return a Bean Step 06 - Enhancing the Hello World Service with a Path Variable

---
+++ src/main/java/com/personal/kunj/springbootrestfulservice/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.personal.kunj.springbootrestfulservice;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	// getMessage is required otherwise we will get error
+	public String getMessage() {
+		return message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+}
--- src/main/java/com/personal/kunj/springbootrestfulservice/HelloWorldController.java
+++ src/main/java/com/personal/kunj/springbootrestfulservice/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.personal.kunj.springbootrestfulservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +13,15 @@ public class HelloWorldController {
 	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean");
+	}
+
+	@GetMapping(path = "/hello-world/path-var/{name}")
+	public HelloWorldBean helloWorldWithPath(@PathVariable("name") String myName) {
+		return new HelloWorldBean(String.format("Hello World, %s", myName));
 	}
 }
