95153ad06a0775ed35bb298be4b59c4658743bd7
KB <kb@kb.com>
2018-08-26 16:51:28 +0530

    Step 15 - Implementing Validations for RESTful Services

--- src/main/java/com/personal/kunj/springbootrestfulservice/user/User.java
+++ src/main/java/com/personal/kunj/springbootrestfulservice/user/User.java
@@ -2,12 +2,15 @@ package com.personal.kunj.springbootrestfulservice.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
 public class User {
 
 	private Integer id;
-
+	@Size(min = 2, message = "Name should have at least 2 characters")
 	private String name;
-
+	@Past
 	private Date birthDate;
 
 	// Must have for REST to convert json data to a java object
--- src/main/java/com/personal/kunj/springbootrestfulservice/user/UserResource.java
+++ src/main/java/com/personal/kunj/springbootrestfulservice/user/UserResource.java
@@ -3,6 +3,8 @@ package com.personal.kunj.springbootrestfulservice.user;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -37,7 +39,7 @@ public class UserResource {
 	// output --> CREATED (status) and URI of the created resource (Location header
 	// will have the uri of the new resource)
 	@PostMapping(path = "/users")
-	public ResponseEntity<Object> createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User newUser = service.save(user);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId())
 				.toUri();
