7fc3ace4109b46878338f86cd8ef028ad25a5202
KB <kb@kb.com>
2018-08-26 13:11:27 +0530

    Step 12 - Implementing Generic Exception Handling for all Resources

---
+++ src/main/java/com/personal/kunj/springbootrestfulservice/exception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.personal.kunj.springbootrestfulservice.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.personal.kunj.springbootrestfulservice.user.UserNotFoundException;
+
+// To share this exception across controllers
+@ControllerAdvice
+// As it is providing response
+@RestController
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class) // To handle all the exceptions
+	public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+		// We want to return our exception response back
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@ExceptionHandler(UserNotFoundException.class) // To handle all the exceptions
+	public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+		// We want to return our exception response back
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(),
+				request.getDescription(false));
+		return new ResponseEntity<Object>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+}
