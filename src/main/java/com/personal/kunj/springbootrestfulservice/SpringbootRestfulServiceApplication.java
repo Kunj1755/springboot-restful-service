5013a98513d59738ed1cbdbb63875fa8461fe08d
KB <kb@kb.com>
2018-08-26 23:59:12 +0530

    Step 18 - Internationalization for RESTful Services

--- src/main/java/com/personal/kunj/springbootrestfulservice/SpringbootRestfulServiceApplication.java
+++ src/main/java/com/personal/kunj/springbootrestfulservice/SpringbootRestfulServiceApplication.java
@@ -1,12 +1,32 @@
 package com.personal.kunj.springbootrestfulservice;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class SpringbootRestfulServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestfulServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+		resourceBundleMessageSource.setBasename("messages");
+		return resourceBundleMessageSource;
 	}
 }
