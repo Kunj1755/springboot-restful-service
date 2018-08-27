12205d19efa719eceb6be091803101d26c95ed34
KB <kb@kb.com>
2018-08-27 22:56:27 +0530

    Step 25 - Implementing Dynamic Filtering for RESTful Service

--- src/main/java/com/personal/kunj/springbootrestfulservice/filtering/FilteringController.java
+++ src/main/java/com/personal/kunj/springbootrestfulservice/filtering/FilteringController.java
@@ -1,16 +1,26 @@
 package com.personal.kunj.springbootrestfulservice.filtering;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 
-	// field1,field2
+	// We only want to send field1,field2 in the response
 	@GetMapping("/filtering")
-	public SomeBean retrieveSomeBean() {
-		return new SomeBean("value1", "value2", "value3");
-
+	public MappingJacksonValue retrieveSomeBean() {
+		SomeBean someBean = new SomeBean("value1", "value2", "value3");
+		// Filter out all the fields in the response except field1 and field2
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter);
+		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+		mapping.setFilters(filters);
+		return mapping;
 	}
 
 }
